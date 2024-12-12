43f7d59468c663dece7d19e0cf591ce8152f9f9b
taylorwaddell <Taylor.Waddell@verisglobal.com>
2024-12-12 08:46:24 -0600

    Initial Commit

---
+++ mindrop/mindropApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindropApp.swift
+//  mindrop
+//
+//  Created by Taylor Waddell on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct mindropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mindropTests/mindropTests.swift
@@ -0,0 +1,17 @@
+//
+//  mindropTests.swift
+//  mindropTests
+//
+//  Created by Taylor Waddell on 12/12/24.
+//
+
+import Testing
+@testable import mindrop
+
+struct mindropTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
