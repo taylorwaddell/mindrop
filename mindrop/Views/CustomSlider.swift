235461a0df1e668fd5cd9bda1f07d18af7db064e
taylorwaddell <Taylor.Waddell@verisglobal.com>
2024-12-13 16:50:15 -0600

    More efficient track updates.

--- mindrop/Views/CustomSlider.swift
+++ mindrop/Views/CustomSlider.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 
-/// A custom slider with a gradient track and draggable handle.
 struct CustomSlider: View {
     @Binding var value: Double
     let maxValue: Double
@@ -24,30 +23,35 @@ struct CustomSlider: View {
                 )
                 .frame(height: 14)
                 .cornerRadius(5)
-                .gesture(
-                    DragGesture(minimumDistance: 0)
-                        .onChanged { gesture in
-                            let dragX = min(max(gesture.location.x, 0), geometry.size.width)
-                            value = round(Double(dragX / geometry.size.width) * maxValue)
-                            onEditingChanged()
-                        }
-                )
 
-                // Draggable handle
-                RoundedRectangle(cornerRadius: 5)
+                RoundedRectangle(cornerRadius: 3)
                     .frame(width: 12, height: 24)
                     .foregroundColor(.white)
                     .shadow(radius: 2)
-                    .offset(x: handlePosition(for: geometry.size.width))
-                    .gesture(
-                        DragGesture()
-                            .onChanged { gesture in
-                                let dragX = min(max(gesture.location.x, 0), geometry.size.width)
-                                value = round(Double(dragX / geometry.size.width) * maxValue)
-                                onEditingChanged()
-                            }
+                    .overlay(
+                        Rectangle()
+                            .frame(width: 2, height: 14)
+                            .foregroundColor(.gray)
+                            .cornerRadius(3)
                     )
+                    .offset(x: handlePosition(for: geometry.size.width))
             }
+            .frame(height: 24)
+            .contentShape(Rectangle())
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged { gesture in
+                        let dragX = min(max(gesture.location.x, 0), geometry.size.width)
+                        let newValue = round(Double(dragX / geometry.size.width) * maxValue)
+                        
+                        if newValue != value {
+                            withTransaction(Transaction(animation: .none)) {
+                                value = newValue
+                            }
+                            onEditingChanged()
+                        }
+                    }
+            )
         }
         .frame(height: 24)
         .padding(.horizontal)
